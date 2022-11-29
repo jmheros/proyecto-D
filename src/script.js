6023ec17fc09f9766cb49e818d82f1627adeb2bd
Jose Maria <jmdelosherosl@icloud.com>
2022-11-29 13:51:07 -0300

    subiendo

---
+++ src/script.js
@@ -0,0 +1,10 @@
+//Toggle button
+function toggleMenu() {
+  var navMenu = document.getElementById("mobile-menu-js");
+  if (navMenu.style.height == "0px") {
+    navMenu.style.height = "134px";     
+  } else {
+    navMenu.style.height = "0px";                  
+  }
+}
+
